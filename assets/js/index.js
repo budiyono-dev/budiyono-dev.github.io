361ecfeb0b63518b53715ab481394d5555353999
budiyono-dev <budiyono-dev@gmail.com>
2022-09-14 15:12:58 +0700

    home

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,43 @@
-function toggleMenu(){
+const menuItem = document.querySelector(".menu-item");
+
+function toggleMenu() {
     document.querySelector(".menu-button").classList.toggle("bg-menu-darker");
-    document.querySelector(".menu-item").classList.toggle("menu-item-reset-margin");
+    menuItem.classList.toggle("menu-item-reset-margin");
+}
+
+function gotoPage(e, page) {
+    e.preventDefault()
+    let newURL;
+    let newTitle;
+    // let newState = { additionalInformation: 'Updated the URL with JS' };
+    switch (page) {
+        case "home":
+            newURL = "/";
+            newTitle = "Home";
+            break;
+        
+        case "posts":
+            newURL = "/posts";
+            newTitle = "Posts";
+            break;
+        
+        case "categories":
+            newURL = "/categories";
+            newTitle = "Categories";
+            break;
+        
+        case "about":
+            newURL = "/about";
+            newTitle = "About";
+            break;
+        
+        default:
+            newURL = "/home";
+            newTitle = "Home";
+            break;
+    }
+
+    history.replaceState({}, "", newURL);
+    document.title = newTitle;
+    menuItem.classList.toggle("menu-item-reset-margin");
 }
