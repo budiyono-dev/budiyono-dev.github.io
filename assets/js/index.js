2853f596796a68575ceecac4d4c27193a234d118
budiyono-dev <budiyono-dev@gmail.com>
2022-09-14 12:49:23 +0700

    reset github page

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+function toggleMenu(){
+    document.querySelector(".menu-button").classList.toggle("bg-menu-darker");
+    document.querySelector(".menu-item").classList.toggle("menu-item-reset-margin");
+}
