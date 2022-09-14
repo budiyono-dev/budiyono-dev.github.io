6580742a9fbc0b28aa94d6967a974a9b4b83ed5f
budiyono-dev <budiyono-dev@gmail.com>
2022-09-14 18:50:42 +0700

    menu active

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,6 +7,10 @@ function toggleMenu() {
 
 function gotoPage(e, page) {
     e.preventDefault()
+    console.log(e.target);
+    inactiveAllMenuItem();
+    e.target.classList.toggle("menu-item-active")
+
     let newURL;
     let newTitle;
     // let newState = { additionalInformation: 'Updated the URL with JS' };
@@ -40,4 +44,12 @@ function gotoPage(e, page) {
     history.replaceState({}, "", newURL);
     document.title = newTitle;
     menuItem.classList.toggle("menu-item-reset-margin");
+}
+
+function inactiveAllMenuItem(){
+    document.querySelectorAll(".menu-item>ul>li>a").forEach(el => {
+        if(el.classList.contains("menu-item-active")){
+            el.classList.toggle("menu-item-active")
+        }
+    });
 }
