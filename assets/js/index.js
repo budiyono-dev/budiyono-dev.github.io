16def95b492baf960f748a3f8d1dd895eb4ad653
budiyono-dev <budiyono-dev@gmail.com>
2022-09-15 17:52:16 +0700

    refactor function

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,12 +5,32 @@ function toggleMenu() {
     menuItem.classList.toggle("menu-item-reset-margin");
 }
 
+function hideMenu(){
+    menuItem.classList.toggle("menu-item-reset-margin");
+}
+
+function activateMenuItem(menuItem){
+    menuItem.classList.toggle("menu-item-active");
+}
+
 function gotoPage(e, page) {
     e.preventDefault()
-    console.log(e.target);
-    inactiveAllMenuItem();
-    e.target.classList.toggle("menu-item-active")
 
+    inactiveAllMenuItem();
+    activateMenuItem(e.target);
+    hideMenu();
+    changeTitleAndURL(page);
+}
+
+function inactiveAllMenuItem() {
+    document.querySelectorAll(".menu-item>ul>li>a").forEach(el => {
+        if (el.classList.contains("menu-item-active")) {
+            el.classList.toggle("menu-item-active")
+        }
+    });
+}
+
+function changeTitleAndURL(page) {
     let newURL;
     let newTitle;
     // let newState = { additionalInformation: 'Updated the URL with JS' };
@@ -19,22 +39,22 @@ function gotoPage(e, page) {
             newURL = "/";
             newTitle = "Home";
             break;
-        
+
         case "posts":
             newURL = "/posts";
             newTitle = "Posts";
             break;
-        
+
         case "categories":
             newURL = "/categories";
             newTitle = "Categories";
             break;
-        
+
         case "about":
             newURL = "/about";
             newTitle = "About";
             break;
-        
+
         default:
             newURL = "/home";
             newTitle = "Home";
@@ -43,13 +63,4 @@ function gotoPage(e, page) {
 
     history.replaceState({}, "", newURL);
     document.title = newTitle;
-    menuItem.classList.toggle("menu-item-reset-margin");
-}
-
-function inactiveAllMenuItem(){
-    document.querySelectorAll(".menu-item>ul>li>a").forEach(el => {
-        if(el.classList.contains("menu-item-active")){
-            el.classList.toggle("menu-item-active")
-        }
-    });
 }
